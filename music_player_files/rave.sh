b7acae3cb0209e1601d6a4ab6051401715ad12a9
Matthew Lundquist <lundquistecologylab@gmail.com>
2022-12-02 16:02:01 -0500

    music update

--- music_player_files/rave.sh
+++ music_player_files/rave.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 play /home/USERNAME/Music/crab_rave.mp3 trim 01:10 fade 3 15 3 norm
+
+# You will also want to set the volume you want for your speaker using amixer
+# e.g. "amixer set Headphone 60%"
